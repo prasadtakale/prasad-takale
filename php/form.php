fd41cbdcd6eb68f4eb497ff9faf352fb0375dc14
prasadtakale <yatintakale@gmail.com>
2021-03-06 21:10:13 +0530

    Added error message

--- php/form.php
+++ php/form.php
@@ -96,7 +96,8 @@ $welcome_subject = "Thank you for getting in touch!"; //Success Message Subject
 
         }catch(Exception $e)
         {
-            $response['status'] = 'error';
+            //$response['status'] = 'error';
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
         
 
